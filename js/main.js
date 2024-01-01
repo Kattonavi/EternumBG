8ffbcaae2d17e9b82ccec982b1062b6956d7ece5
Kattonavi <yt731318@gmail.com>
2024-01-01 15:40:35 -0500

    V 1.2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// main.js
+
+// Función para abrir el modal
+function openModal(modalName) {
+    var modal = document.getElementById(modalName + 'Modal');
+    modal.style.display = 'block';
+  }
+  
+  // Función para cerrar el modal
+  function closeModal(modalName) {
+    var modal = document.getElementById(modalName + 'Modal');
+    modal.style.display = 'none';
+  }
+  
